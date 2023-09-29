a95afa521c16eba5da9a2982812e01f2d01fd867
Joey Palchak <joey.palchak@gmail.com>
2023-09-29 08:32:18 -0700

    add tests for Order getters

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,62 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("Bread Order", "10 Orders at $5 each.", 50, "9/29/2023");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnsOrderTitle_String()
+    {
+      string title = "Bread Order";
+      Order newOrder = new Order(title, "10 Orders at $5 each.", 50, "9/29/2023");
+
+      string result = newOrder.Title;
+
+      Assert.AreEqual(title, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsOrderDescription_String()
+    {
+      string description = "10 Orders at $5 each.";
+      Order newOrder = new Order("Bread Order", description, 50, "9/29/2023");
+
+      string result = newOrder.Description;
+
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPriceOfOrder_Float()
+    {
+      float price = 50.50f;
+      Order newOrder = new Order("Bread Order", "10 Orders.", price, "9/29/2023");
+
+      float result = newOrder.Price;
+
+      Assert.AreEqual(price, result);
+    }
+
+    [TestMethod]
+    public void GetDate_ReturnsDateOfOrder_String()
+    {
+      string date = "9/29/2023";
+      Order newOrder = new Order("Bread Order", "10 Orders.", 50, date);
+
+      string result = newOrder.Date;
+
+      Assert.AreEqual(date, result);
+    }
+
+    // [TestMethod]
+    // public void GetAll_ReturnsEmptyList_OrderList()
+    // {
+    //   List<Order> newList = new List<Order> {};
+
+    //   List<Order> result = Order.GetAll();
+
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
   }
 }
