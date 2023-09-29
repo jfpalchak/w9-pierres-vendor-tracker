8cb118fcf3679e56348233b31903db70a693b9cf
Joey Palchak <joey.palchak@gmail.com>
2023-09-29 10:01:08 -0700

    add Create route for VendorsController

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -7,6 +7,7 @@ namespace VendorTracker.Controllers
 {
   public class VendorsController : Controller
   {
+    // Get list of all Vendors to display
     [HttpGet("/vendors")]
     public ActionResult Index()
     {
@@ -14,11 +15,21 @@ namespace VendorTracker.Controllers
       return View(allVendors);
     }
 
+    // Get the form for a New Vendor
     [HttpGet("/vendors/new")]
     public ActionResult New()
     {
       return View();
     }
     
+    // Upon submitting the form, Create the new Vendor and redirect to root path
+    // (CR asks for user to be routed back to the "homepage" - which homepage? Assuming Vendor homepage.)
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
   }
 }
