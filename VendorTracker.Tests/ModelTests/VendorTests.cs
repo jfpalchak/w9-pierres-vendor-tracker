f97db76b1b9e2933c70160113f1572915f18e9e8
Joey Palchak <joey.palchak@gmail.com>
2023-09-29 08:58:44 -0700

    add test for Vendor Description getter

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -11,19 +11,33 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Name");
+      Vendor newVendor = new Vendor("Name", "Description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
     [TestMethod]
     public void GetName_ReturnsVendorName_String()
     {
-      string name = "Sally";
-      Vendor newVendor = new Vendor(name);
+      string name = "Sally's";
+      Vendor newVendor = new Vendor(name, "Description");
 
       string result = newVendor.Name;
 
       Assert.AreEqual(name, result);
     }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescriptionOfVendor_String()
+    {
+      string description = "Sally's Coffee Shop, a bi-weekly Order";
+      Vendor newVendor = new Vendor("Sally's", description);
+
+      string result = newVendor.Description;
+
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    
   }
 }
