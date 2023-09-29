8fa19a0c6ff4b695e924b7e6a557f50ee23f929a
Joey Palchak <joey.palchak@gmail.com>
2023-09-29 08:32:46 -0700

    create Order constructor + pass initial tests

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -3,6 +3,19 @@ namespace VendorTracker.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public float Price { get; set; }
+    public string Date { get; set; }
 
+    // private static List<Order> _instances = new List<Order> {};
+
+    public Order(string orderTitle, string orderDescription, float orderPrice, string orderDate)
+    {
+      Title = orderTitle;
+      Description = orderDescription;
+      Price = orderPrice;
+      Date = orderDate;
+    }
   }
 }
