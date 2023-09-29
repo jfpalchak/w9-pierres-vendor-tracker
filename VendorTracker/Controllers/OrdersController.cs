ff3fec8487f0071586426ce6940eea24406605bb
Joey Palchak <joey.palchak@gmail.com>
2023-09-29 14:25:15 -0700

    add Edit & Update routes to OrdersController

--- VendorTracker/Controllers/OrdersController.cs
+++ VendorTracker/Controllers/OrdersController.cs
@@ -29,5 +29,39 @@ namespace VendorTracker.Controllers
       return View(foundVendor);
     }
 
+    // Get the form to Update a specified Order
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}/edit")]
+    public ActionResult Edit(int vendorId, int orderId)
+    {
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order foundOrder = Order.Find(orderId);
+
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      
+      model.Add("vendor", foundVendor);
+      model.Add("order", foundOrder);
+
+      return View(model);
+    }
+
+    // Upon submitting Edit form, update the specified Order's details and display that order's page
+    [HttpPost("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Update(int vendorId, int orderId, string newTitle, string newDescription, float newPrice, string newDate)
+    {
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order foundOrder = Order.Find(orderId);
+
+      foundOrder.Title = newTitle;
+      foundOrder.Description = newDescription;
+      foundOrder.Price = newPrice;
+      foundOrder.Date = newDate;
+
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      
+      model.Add("vendor", foundVendor);
+      model.Add("order", foundOrder);
+
+      return View("Show", model);
+    }
   }
 }
