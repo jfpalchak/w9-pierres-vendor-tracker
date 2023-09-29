2e59b76a74fd8c0cabcd80f590908add1c0e8f3c
Joey Palchak <joey.palchak@gmail.com>
2023-09-29 10:45:05 -0700

    add OrdersController routes for Show and New

--- VendorTracker/Controllers/OrdersController.cs
+++ VendorTracker/Controllers/OrdersController.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using VendorTracker.Models;
 
 namespace VendorTracker.Controllers
 {
   public class OrdersController : Controller
   {
-    // [Route("/route decoration")]
-    // public MethodToCall()
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order foundOrder = Order.Find(orderId);
+
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      
+      model.Add("vendor", foundVendor);
+      model.Add("order", foundOrder);
+      return View(model);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor foundVendor = Vendor.Find(vendorId);
+
+      return View(foundVendor);
+    }
 
   }
 }
