c7f88f581f174b4bb13822970ecee3f818d2851a
Joey Palchak <joey.palchak@gmail.com>
2023-09-29 08:49:49 -0700

    add initial tests for Vendor class

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -8,6 +8,22 @@ namespace VendorTracker.Tests
   [TestClass]
   public class VendorTests
   {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Name");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
 
+    [TestMethod]
+    public void GetName_ReturnsVendorName_String()
+    {
+      string name = "Sally";
+      Vendor newVendor = new Vendor(name);
+
+      string result = newVendor.Name;
+
+      Assert.AreEqual(name, result):
+    }
   }
 }
