d2170d9c4db91f2bd4fd8243cd60af57679ca9f5
Joey Palchak <joey.palchak@gmail.com>
2023-09-29 08:17:45 -0700

    add basic using directives

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -1,9 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using VendorTracker.Models;
 
 namespace VendorTracker.Tests
 {
   [TestClass]
   public class OrderTests
   {
-    
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
 }
--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -1,9 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using VendorTracker.Models;
 
 namespace VendorTracker.Tests
 {
   [TestClass]
   public class VendorTests
   {
-    
+
   }
 }
